bddb12843de6bb4a22c604c94a7c3bc4622c10f5
kekedezha <dezhac6@gmail.com>
2024-06-06 16:42:56 -0400

    updated header, working on nav component....

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,13 +12,20 @@ const Header = ({viewCart, setViewCart}: PropsType) => {
     const content = (
         <header className="header">
             <div className="header__title-bar">
-                <h1>GOAT 'REIMAGINED'</h1>
+                <h1 className="header__title-name">GOAT 'REIMAGINED'</h1>
                 <div className="header__price-box">
-                    <p>Total Items: {totalItems}</p>
-                    <p>Total Price: {totalPrice}</p>
+                    <div className="price-box">
+                        <p>Total Items: {totalItems}</p>
+                        <p>Total Price: {totalPrice}</p>
+                    </div>
+                    {viewCart ? 
+                        <button className="btn btn-neutral" title="View Products" onClick={() => setViewCart(false)}>View Products</button>
+                    :
+                        <button className="btn btn-ghost" title="View Cart" onClick={() => setViewCart(true)}>🛒</button>
+                    }
                 </div>
             </div>
-            <Nav viewCart={viewCart} setViewCart={setViewCart} />
+            <Nav />
         </header>
     )
 
