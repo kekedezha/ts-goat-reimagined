da2e09ddc2f57608855fd95369ca979224ea9001
kekedezha <dezhac6@gmail.com>
2024-06-14 21:57:52 -0400

    added new breakpoints to add responsiveness to page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'tablet': '768px',
+      // => @media (min-width: 768px)
+      // 768 <= screen size < 1023 
+      'mobileLarge': '425px',
+      // => @media (min-width: 425) 
+      // 425 <= screen size < 767
+      'mobileMed': '375px',
+      // => @media (min-width: 375)
+      // 375 <= screen size < 424
+    },
     extend: {},
   },
   plugins: [
